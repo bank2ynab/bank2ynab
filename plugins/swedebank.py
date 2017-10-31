7530bdf9e627dbc309d6bea5009a99a6462b3e17
toyg <g.lacava@gmail.com>
2017-10-31 12:45:05 +0000

    added plugin for Swedebank

--- plugins/swedebank.py
+++ plugins/swedebank.py
@@ -1,23 +1,38 @@
 # with thanks to https://github.com/jpsk/Swedbank-to-YNAB4-converter/
+
 from bank2ynab import B2YBank, CrossversionCsvReader
 
 
-class SwedbankPlugin(B2YBank):
+class SwedebankPlugin(B2YBank):
+    def __init__(self, config_object, is_py2):
+        super(SwedebankPlugin, self).__init__(config_object, is_py2)
+        self.name = "SwedeBank"
+
     def read_data(self, file_path):
         delim = self.config["input_delimiter"]
         output_columns = self.config["output_columns"]
+        # we know it should have headers, but we respect the setting
+        has_headers = self.config["has_headers"]
         output_data = []
 
         with CrossversionCsvReader(file_path,
                                    self._is_py2,
                                    delimiter=delim) as reader:
-            for row in reader:
+            for index, row in enumerate(reader):
+                # skip first row if headers
+                if index == 0 and has_headers:
+                    continue
                 tmp = {}
                 date = row[2].split('-')
-                tmp["Date"] = date[0] + '/' + date[1] + '/' + date[2]
-                tmp["Payee"] = ''  # wat?
+                tmp["Date"] = date[2] + '/' + date[1] + '/' + date[0]
+                tmp["Payee"] = row[3]
                 tmp["Category"] = ''
-                tmp["Memo"] = row[4]  # this is probably the payee...
+                tmp["Memo"] = row[4]
+                # if the record ID is empty, it's some sort of balance, ignore
+                if row[8] == "":
+                    continue
+
+                # K is inflow, D is outflow - I guess Kredit and Debit ;)
                 if row[7] == 'K':
                     tmp["Outflow"] = ''
                     tmp["Inflow"] = row[5]
@@ -26,12 +41,12 @@ class SwedbankPlugin(B2YBank):
                     tmp["Inflow"] = ''
 
                 # respect Output Columns option
-                out_row = []
+                out_row = [''] * len(output_columns)
                 for index, key in enumerate(output_columns):
-                    out_row[key] = tmp.get(key, "")
+                    out_row[index] = tmp.get(key, "")
                 output_data.append(out_row)
         return output_data
 
 
 def build_bank(config, is_py2):
-    return SwedbankPlugin(config, is_py2)
+    return SwedebankPlugin(config, is_py2)

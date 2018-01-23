6ec61d50dbeca4c31dccddfdbfb29afd8e86c4c3
Torben Gundtofte-Bruun <torbengb@users.noreply.github.com>
2018-01-23 17:11:42 +0100

    Create nl-rabobank.py
    This addresses Issue 90. It's based on the Swedbank plugin - I hope I got it right!

---
+++ plugins/nl-rabobank.py
@@ -0,0 +1,55 @@
+# with thanks to https://github.com/jpsk/Swedbank-to-YNAB4-converter/
+
+from bank2ynab import B2YBank, CrossversionCsvReader
+
+class NLRabobankPlugin(B2YBank):
+    def __init__(self, config_object, is_py2):
+        super(NLRabobankPlugin, self).__init__(config_object, is_py2)
+        self.name = "NL Rabobank"
+
+    def read_data(self, file_path):
+        delim = self.config["input_delimiter"]
+        output_columns = self.config["output_columns"]
+        # we know it should have headers, but we respect the setting
+        has_headers = self.config["has_headers"]
+        output_data = []
+
+        with CrossversionCsvReader(file_path,
+                                   self._is_py2,
+                                   delimiter=delim) as reader:
+            for index, row in enumerate(reader):
+                # skip first row if headers
+                if index == 0 and has_headers:
+                    continue
+                tmp = {}
+# I assume that "[2]" means "field 2 in the current row".
+# DATE STUFF:
+# YNAB's date format is "DD/MM/YYYY". 
+# This bank's date format is "YYYMMDD" without delimiters.
+                date = row[2].split('-')
+                tmp["Date"] = date[2] + '/' + date[1] + '/' + date[0]
+# PAYEE STUFF:
+                tmp["Payee"] = row[7]
+# CATEGORY STUFF:
+                tmp["Category"] = ''
+# MEMO STUFF:
+                tmp["Memo"] = row[11]
+# AMOUNT STUFF:
+                # C means inflow (credit), D means outflow (debit)
+                if row[4] == 'C':
+                    tmp["Outflow"] = ''
+                    tmp["Inflow"] = row[5]
+                else:
+                    tmp["Outflow"] = row[5]
+                    tmp["Inflow"] = ''
+
+                # respect Output Columns option
+                out_row = [''] * len(output_columns)
+                for index, key in enumerate(output_columns):
+                    out_row[index] = tmp.get(key, "")
+                output_data.append(out_row)
+        return output_data
+
+
+def build_bank(config, is_py2):
+    return NLRabobankPlugin(config, is_py2)

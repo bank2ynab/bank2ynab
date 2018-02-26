38bc325507a1cc3cbc2a07bd3cd2b5a3e666a942
Torben Gundtofte-Bruun <torbengb@users.noreply.github.com>
2018-02-26 14:20:52 +0100

    flake8 corrections

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-    # Package meta-data:
+# Package meta-data:
 NAME = "bank2ynab"
 DESCRIPTION = "A common project to consolidate all conversion efforts from various banks' export formats into YNAB's import format."
 LONG_DESCRIPTION = "A common project to consolidate all conversion efforts from various banks' export formats into YNAB's import format."
@@ -6,19 +6,19 @@ URL = "https://github.com/torbengb/bank2ynab"
 EMAIL = "torben@g-b.dk"
 AUTHOR = "https://github.com/torbengb/bank2ynab/graphs/contributors"
 
-    # define the version numbers:
+# define the version numbers:
 version_major = 1 # must be integer
 version_minor = 1 # must be integer
 version_patch = "0" # must be string
 version_suffix = "mytestbranch" # I'd like to *automagically* include the GitHub branch here.
 
-    # proper release should not have a version suffix:
+# proper release should not have a version suffix:
 if version_suffix == "master": 
-  version_suffix = ""
-    # if it's not a master release then add the suffix to the patch level:
+    version_suffix = ""
+# if it's not a master release then add the suffix to the patch level:
 if version_suffix != "": 
-  version_patch = version_patch + '-' + version_suffix
+    version_patch = version_patch + '-' + version_suffix
 
-    # version number according to https://codereview.stackexchange.com/a/131490 :
+# version number according to https://codereview.stackexchange.com/a/131490 :
 version_info = (version_major, version_minor, version_patch)
 VERSION = '.'.join(str(c) for c in version_info)

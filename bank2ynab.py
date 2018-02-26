3424e64dff12eb135f922c36162c9c5d15ac400f
Torben Gundtofte-Bruun <torben@g-b.dk>
2018-02-26 23:37:42 +0100

    "startswith" filename search
    This works without regex. With regex it doesn't, so I've commented it out.

--- bank2ynab.py
+++ bank2ynab.py
@@ -328,12 +328,18 @@ class B2YBank(object):
                 directory_list = os.listdir(".")
             if regex_active is True:
                 files = [join(path, f)
-                         for f in directory_list if f.endswith(ext)
-                         if re.search(file_pattern, f) if prefix not in f]
+                         for f in directory_list 
+                         if f.endswith(ext)
+                         if f.startswith(file_pattern)
+                         if re.search(file_pattern, f) 
+                         if prefix not in f]
             else:
                 files = [join(path, f)
-                         for f in directory_list if f.endswith(ext)
-                         if file_pattern in f if prefix not in f]
+                         for f in directory_list 
+                         if f.endswith(ext)
+                         if f.startswith(file_pattern)
+                         if file_pattern in f 
+                         if prefix not in f]
             if files != [] and missing_dir is True:
                 s = ("\nFormat: {}\n\nError: Can't find download path: {}"
                      "\nTrying default path instead:\t {}")

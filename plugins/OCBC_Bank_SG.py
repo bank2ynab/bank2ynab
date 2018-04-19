6fc108948571dfb4df852287b374a23a88d2fa4b
Junxuan <njunxuan@gmail.com>
2018-04-19 11:18:29 +0800

    Implements preprocessing plugin for OCBC Bank

--- plugins/OCBC_Bank_SG.py
+++ plugins/OCBC_Bank_SG.py
@@ -3,7 +3,7 @@
 from bank2ynab import B2YBank
 
 
-class NullBank(B2YBank):
+class OCBC_Bank_SG(B2YBank):
     """ Example subclass used for testing the plugin system."""
     def __init__(self, config_object, is_py2):
         """
@@ -11,8 +11,8 @@ class NullBank(B2YBank):
         :param is_py2: boolean indicating if we're running under
                         Python 2.x
         """
-        super(NullBank, self).__init__(config_object, is_py2)
-        self.name = "NullBank"
+        super(OCBC_Bank_SG, self).__init__(config_object, is_py2)
+        self.name = "OCBC_Bank_SG"
 
     def _preprocess_file(self, file_path):
         """
@@ -21,6 +21,33 @@ class NullBank(B2YBank):
         :param file_path: path to file
         """
         # what do we actually want to do?
+        header_rows = int(self.config["header_rows"])
+        footer_rows = int(self.config["footer_rows"])
+
+        # get total number of rows in transaction file using a generator
+        with open(file_path) as row_counter:
+            row_count = sum(1 for _ in row_counter)
+
+        with open(file_path) as input_file:
+            output_rows = []
+            for rownum, row in enumerate(input_file):
+                # append headers and footers without modification
+                if rownum < header_rows or rownum > (row_count - footer_rows):
+                    output_rows.append(row)
+                    continue
+                if row[0] == ",":
+                    # join with the previous row but excluding the newline char
+                    # of the previous row
+                    output_rows[-1] = output_rows[-1][:-1] \
+                        + ',' + row.strip(' ,')
+                else:
+                    output_rows.append(row)
+
+        # overwrite source file
+        with open(file_path, 'w') as output_file:
+            for row in output_rows:
+                output_file.write(row)
+
         return
 
 
@@ -33,4 +60,4 @@ def build_bank(config, is_py2):
     :param is_py2: boolean indicating whether we are running under Python 2.x
     :return: a B2YBank subclass instance
     """
-    return NullBank(config, is_py2)
+    return OCBC_Bank_SG(config, is_py2)

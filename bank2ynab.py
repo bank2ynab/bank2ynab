1f97cdc64d76a063826eecf558263b11aee52efa
nocalla <niallwocallaghan@gmail.com>
2018-11-25 00:14:36 +0000

    Use raw string in regex
    Fix W605 invalid escape sequence error

--- bank2ynab.py
+++ bank2ynab.py
@@ -334,7 +334,7 @@ class B2YBank(object):
                 files = [join(path, f)
                          for f in directory_list
                          if f.endswith(ext)
-                         if re.match(file_pattern + ".*\.", f)
+                         if re.match(file_pattern + r".*\.", f)
                          if prefix not in f]
             else:
                 files = [join(path, f)

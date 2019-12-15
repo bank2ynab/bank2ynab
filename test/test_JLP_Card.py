cf948e6f56a5c7792a24c57aaffd2d768155af45
Clancy Childs <clancy.childs@dowjones.com>
2019-12-15 00:03:49 +0000

    fix flake8 tests

--- test/test_JLP_Card.py
+++ test/test_JLP_Card.py
@@ -1,15 +1,13 @@
-from copy import copy
 from unittest import TestCase
 
-from os.path import join, abspath, exists
+from os.path import join
 
-import os
-
-from bank2ynab import B2YBank, fix_conf_params, build_bank
+from bank2ynab import fix_conf_params
 from plugins.JLP_Card_UK import JLP_Card_UKPlugin
 from test.utils import get_test_confparser
 
 _PY2 = False
+
 
 class TestJLP_Card_UKPlugin(TestCase):
 
@@ -25,12 +23,13 @@ class TestJLP_Card_UKPlugin(TestCase):
     def test_read_data(self):
         """ Test that the right number of rows are read from the test files """
         # if you need more tests, add sections to test.conf & specify them here
-        for section_name, num_records, fpath in [
-                ("test_jlp_card", 11, "MS_JANE_SMITH_01-12-2019_14-12-2019.csv")
-        ]:
-            config = fix_conf_params(self.cp, section_name)
-            b = JLP_Card_UKPlugin(config, self.py2)
-            records = b.read_data(join("test-data", fpath))
-            self.assertEqual(len(records), num_records)
-            self.assertEqual(records[10][5], "1100.00")
-            self.assertEqual(records[4][4],"80.99")
+        (section_name,
+         num_records,
+         fpath) = ("test_jlp_card", 11,
+                   "MS_JANE_SMITH_01-12-2019_14-12-2019.csv")
+        config = fix_conf_params(self.cp, section_name)
+        b = JLP_Card_UKPlugin(config, self.py2)
+        records = b.read_data(join("test-data", fpath))
+        self.assertEqual(len(records), num_records)
+        self.assertEqual(records[10][5], "1100.00")
+        self.assertEqual(records[4][4], "80.99")

3e68430d0a194e24205b248dfa9db8613c00762b
Niall O'Callaghan <nocalla@users.noreply.github.com>
2020-01-06 14:18:33 +0000

    Little explanation

--- plugins/plugin-template.py
+++ plugins/plugin-template.py
@@ -12,6 +12,12 @@ class YourActualBankPlugin(B2YBank):
 
     def _preprocess_file(self, file_path):
         """
+        This is an example of how to preprocess the transaction file
+        prior to feeding the data into the main read_data function.
+        Any specialised string or format operations can easily
+        be done here.
+        """
+        """
         For every row that doesn't have a valid date field
         strip out separators and append to preceding row.
         Overwrite input file with modified output.

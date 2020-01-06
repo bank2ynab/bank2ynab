bf9bb5a0a29b4a85a33acd4e59f70e827d008929
Niall O'Callaghan <nocalla@users.noreply.github.com>
2020-01-06 14:17:03 +0000

    Update to use preferred _preprocess_file method

--- plugins/plugin-template.py
+++ plugins/plugin-template.py
@@ -10,53 +10,41 @@ class YourActualBankPlugin(B2YBank):
         super(YourActualBankPlugin, self).__init__(config_object, is_py2)
         self.name = "YourActualBank"
 
-    def read_data(self, file_path):
-        delim = self.config["input_delimiter"]
-        output_columns = self.config["output_columns"]
-        # we know it should have headers, but we respect the setting
-        header_rows = self.config["header_rows"]
-        output_data = []
+    def _preprocess_file(self, file_path):
+        """
+        For every row that doesn't have a valid date field
+        strip out separators and append to preceding row.
+        Overwrite input file with modified output.
+        :param file_path: path to file
+        """
+        # what do we actually want to do?
+        header_rows = int(self.config["header_rows"])
+        footer_rows = int(self.config["footer_rows"])
 
-        with CrossversionCsvReader(file_path,
-                                   self._is_py2,
-                                   delimiter=delim) as reader:
-            for index, row in enumerate(reader):
-                # skip first row if headers
-                if index == 0 and header_rows != 0:
+        # get total number of rows in transaction file using a generator
+        with open(file_path) as row_counter:
+            row_count = sum(1 for _ in row_counter)
+
+        with open(file_path) as input_file:
+            output_rows = []
+            for rownum, row in enumerate(input_file):
+                # append headers and footers without modification
+                if rownum < header_rows or rownum > (row_count - footer_rows):
+                    output_rows.append(row)
                     continue
-                tmp = {}
-                """
-                DATE STUFF:
-                YNAB's date format is "DD/MM/YYYY".
-                This bank's date format is "YYYMMDD" without delimiters.
-                Moving the substrings into the proper order:
-                https://stackoverflow.com/a/663175/20571
-                """
-                date = row[2]
-                tmp["Date"] = date[6:7] + '/' + date[4:5] + '/' + date[0:3]
-                # PAYEE STUFF:
-                tmp["Payee"] = row[7]
-                # CATEGORY STUFF:
-                tmp["Category"] = ''
-                # MEMO STUFF:
-                tmp["Memo"] = row[11]
-                # AMOUNT STUFF:
-                # C means inflow (credit), D means outflow (debit)
-                if row[4] == 'C':
-                    tmp["Outflow"] = ''
-                    tmp["Inflow"] = row[5]
+                if row[0] == ",":
+                    # join with the previous row but excluding the newline char
+                    # of the previous row
+                    output_rows[-1] = output_rows[-1][:-1] \
+                        + ',' + row.strip(' ,')
                 else:
-                    tmp["Outflow"] = row[5]
-                    tmp["Inflow"] = ''
+                    output_rows.append(row)
 
-                # respect Output Columns option
-                out_row = [''] * len(output_columns)
-                for index, key in enumerate(output_columns):
-                    out_row[index] = tmp.get(key, "")
-                output_data.append(out_row)
-            # insert the header row at the top.
-            output_data.insert(0, output_columns)
-        return output_data
+        # overwrite source file
+        with open(file_path, 'w') as output_file:
+            for row in output_rows:
+                output_file.write(row)
+        return
 
 
 def build_bank(config, is_py2):

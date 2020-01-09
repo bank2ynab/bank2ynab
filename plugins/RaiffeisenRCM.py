cc30638ec66d1bb84662c743768db46071a67f9c
lint-action <lint-action@samuelmeuli.com>
2020-01-09 20:06:40 +0000

    Fix code style issues with Black

--- plugins/RaiffeisenRCM.py
+++ plugins/RaiffeisenRCM.py
@@ -15,9 +15,7 @@ class RaiffeisenRCMPlugin(B2YBank):
         header_rows = self.config["header_rows"]
         output_data = []
 
-        with CrossversionCsvReader(
-            file_path, self._is_py2, delimiter=delim
-        ) as reader:
+        with CrossversionCsvReader(file_path, self._is_py2, delimiter=delim) as reader:
             for index, row in enumerate(reader):
                 tmp = {}
 

60664cc68f994299b9dd2290509f4541922027cf
nocalla <nocalla@users.noreply.github.com>
2020-05-08 18:04:29 +0100

    Add get_banks function to return list of banks

--- bank_list_update.py
+++ bank_list_update.py
@@ -2,3 +2,23 @@
 Helper script to update README.md to reflect banks listed in bank2ynab.conf
 """
 
+import re
+
+def get_banks(file):
+    """
+    :param file: filename for config file
+    :return banks: list of bank names
+    """
+    with open(file, "r") as f:
+        file_contents = f.read()    
+    
+    bracket_regex = re.compile("\n\[(.*)\]\n")
+    banks = bracket_regex.findall(file_contents)
+    
+    return banks[1:]
+# Variables
+config_file = "bank2ynab.conf"
+readme_file = "README.md"
+start_token = "<!--AUTO BANK UPDATE START-->"
+end_token = "<!--AUTO BANK UPDATE END-->"
+run(config_file, readme_file, start_token, end_token)

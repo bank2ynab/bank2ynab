dae09576f4334123cb050bfa92f9fc706dc965bb
nocalla <nocalla@users.noreply.github.com>
2020-05-08 19:49:11 +0100

    Black formatting updates

--- bank_list_update.py
+++ bank_list_update.py
@@ -8,6 +8,7 @@ import re
 # configure our logger
 logging.basicConfig(format="%(levelname)s: %(message)s", level=logging.INFO)
 
+
 def get_banks(file):
     """
     :param file: filename for config file
@@ -16,13 +17,14 @@ def get_banks(file):
     logging.info("Reading bank configuration file ({})...".format(file))
 
     with open(file, "r") as f:
-        file_contents = f.read()    
-    
+        file_contents = f.read()
+
     bracket_regex = re.compile("\n\[(.*)\]\n")
     banks = bracket_regex.findall(file_contents)
-    
+
     return banks[1:]
-    
+
+
 def edit_readme(file, start, end, banks):
     """
     :param file: filename for readme file
@@ -34,20 +36,20 @@ def edit_readme(file, start, end, banks):
 
     bank_list_text = start + "\n"
     for bank in banks:
-       bank_list_text += "1. {}\n".format(bank)
+        bank_list_text += "1. {}\n".format(bank)
     bank_list_text += end
-    
+
     with open(file, "r") as f:
         file_contents = f.read()
-      
+
     logging.info("Writing new list to {}".format(file))
 
     with open(file, "w") as f:
         list_regex = re.compile("({})(.*)({})".format(start, end), re.DOTALL)
         file_contents = list_regex.sub(bank_list_text, file_contents)
         f.write(file_contents)
-        
-    
+
+
 def run(read_file, write_file, start, end):
     """
     :param read_file: filename for config file
@@ -58,7 +60,8 @@ def run(read_file, write_file, start, end):
     banks = get_banks(read_file)
     if banks != []:
         edit_readme(write_file, start, end, banks)
-    logging.info("Done.")    
+    logging.info("Done.")
+
 
 # Variables
 config_file = "bank2ynab.conf"

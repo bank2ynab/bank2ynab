46372b0d8a3c55847a195e8b6803f6555507e748
nocalla <nocalla@users.noreply.github.com>
2020-05-08 19:44:14 +0100

    Add edit_readme function

--- bank_list_update.py
+++ bank_list_update.py
@@ -16,6 +16,28 @@ def get_banks(file):
     banks = bracket_regex.findall(file_contents)
     
     return banks[1:]
+def edit_readme(file, start, end, banks):
+    """
+    :param file: filename for readme file
+    :param start: string indicating start of section to replace
+    :param end: string indicating end of section to replace
+    :param banks: list of banks
+    """
+
+    bank_list_text = start + "\n"
+    for bank in banks:
+       bank_list_text += "1. {}\n".format(bank)
+    bank_list_text += end
+    
+    with open(file, "r") as f:
+        file_contents = f.read()
+      
+    logging.info("Writing new list to {}".format(file))
+
+    with open(file, "w") as f:
+        list_regex = re.compile("({})(.*)({})".format(start, end), re.DOTALL)
+        file_contents = list_regex.sub(bank_list_text, file_contents)
+        f.write(file_contents)
 # Variables
 config_file = "bank2ynab.conf"
 readme_file = "README.md"

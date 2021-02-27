04d624ef4bd447473087fab10dca05bd20098e3f
jacob toft pedersen <jacob@rotand.dk>
2021-02-27 17:01:30 +0100

    Import tree
    NB: in a state of works for me on my machine.
    
    But the general idea is specify the bank up front. I found the the pattern matching file finder was
    not suited when i could download with any name for my files
    
    Then import a tree as in
    
    Get all budgets and accounts from ynab api and create a structure like
    
    ```
    imports/
    └── My Budget
        ├── AL-budget
        ├── AL-Helle
        ├── Al-jacob
        └── AL-MadKontoen
    ```
    
    Then walk trough all those direcories and post any bank-transactions into ynab
    
    Finally move the files to a history folder with a similar structure as the import tree, so they are
    only processed once.

--- bank2ynab/__main__.py
+++ bank2ynab/__main__.py
@@ -1,22 +1,71 @@
 import logging
+import os
+from collections import namedtuple
 
+from b2y_utilities import get_configs, get_project_dir
 from bank_process import Bank2Ynab
-from b2y_utilities import get_configs
 from ynab_api import YNAB_API
 
 # configure our logger
 logging.basicConfig(format="%(levelname)s: %(message)s", level=logging.INFO)
 
-# Let's run this thing!
+def get_import_root():
+    return os.path.join(get_project_dir(), "imports")
+
+
+def init_import_root(budgets):
+    """
+    Create a directory structure matching the account names on ynab
+    :return:
+    """
+    root = get_import_root()
+    for b in budgets:
+        for a in b.accounts:
+            os.makedirs(os.path.join(root, b.name, a.name), exist_ok=True)
+
+
+Job = namedtuple("Job", ("budget", "account", "file"))
+
+
+def move_to_history_folder(j):
+    root = os.path.join(get_project_dir(), "history")
+    prefix = ""  # TODO "timestamp-now"
+    folder = os.path.join(root, j.budget.name, j.account.name)
+    os.makedirs(folder, exist_ok=True)
+    name = prefix + os.path.basename(j.file)
+    os.rename(j.file, os.path.join(folder, name))
+
+
+def process_import_tree(bank, budgets, api):
+    root = get_import_root()
+    jobs = []
+    for b in budgets:
+        for a in b.accounts:
+            account_path = os.path.join(root, b.name, a.name)
+            for f in os.listdir(account_path):
+                jobs.append(Job(budget=b, account=a, file=os.path.join(account_path, f)))
+    for j in jobs:
+        transaction_data = bank.read_data(j.file)
+        if not transaction_data:
+            logging.info("No data in file for job: {}".format(j))
+            continue
+        print(transaction_data)
+        api.post_transactions(transaction_data, j.budget.id, j.account.id)
+        move_to_history_folder(j)
+
+
 if __name__ == "__main__":
-    try:
-        config = get_configs()
-    except FileNotFoundError:
-        logging.error("No configuration file found, process aborted.")
-        pass
-    else:
-        b2y = Bank2Ynab(config)
-        b2y.run()
-        api = YNAB_API(config)
-        if b2y.transaction_data:
-            api.run(b2y.transaction_data)
+    bank_id = "DK Bankernes EDB Central"
+    file = "/home/jacob/Downloads/Al_jacob.csv"
+    config = get_configs()
+
+    b2y = Bank2Ynab(config)
+    my_bank = b2y.get_bank_by_name(bank_id)
+    print(my_bank.name)
+    # transaction_data = my_bank.process_file(file)
+    # print(transaction_data)
+    api = YNAB_API(config)
+    budgets = api.list_budgets()
+    print(budgets)
+    init_import_root(budgets)
+    process_import_tree(my_bank, budgets, api)

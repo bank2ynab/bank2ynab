d074e69281339721cd47deaca9244118df643e77
jacob toft pedersen <jacob@rotand.dk>
2021-04-09 20:47:58 +0200

    store account specific bank id as config in json format

--- bank2ynab/__main__.py
+++ bank2ynab/__main__.py
@@ -1,7 +1,9 @@
 import logging
 import os
+import json
 from time import localtime, strftime
 from collections import namedtuple
+
 
 from b2y_utilities import get_configs, get_project_dir
 from bank_process import Bank2Ynab
@@ -9,6 +11,8 @@ from ynab_api import YNAB_API
 
 # configure our logger
 logging.basicConfig(format="%(levelname)s: %(message)s", level=logging.INFO)
+
+CONFIG_FILE="account_info.json"
 
 def get_import_root():
     return os.path.join(get_project_dir(), "imports")
@@ -20,12 +24,17 @@ def init_import_root(budgets):
     :return:
     """
     root = get_import_root()
-    for b in budgets:
-        for a in b.accounts:
-            os.makedirs(os.path.join(root, b.name, a.name), exist_ok=True)
+    for budget in budgets:
+        for account in budget.accounts:
+            folder = os.path.join(root, budget.name, account.name)
+            os.makedirs(folder, exist_ok=True)
+            config = os.path.join(folder, CONFIG_FILE)
+            if not os.path.exists(config):
+                with open(config, "w") as f:
+                    json.dump({"bank_id": "DK Bankernes EDB Central"}, f)
 
 
-Job = namedtuple("Job", ("budget", "account", "filename"))
+Job = namedtuple("Job", ("budget", "account", "filename", "bank_id"))
 
 
 def move_to_history_folder(job):
@@ -37,15 +46,22 @@ def move_to_history_folder(job):
     os.rename(job.filename, os.path.join(folder, name))
 
 
-def process_import_tree(bank, budgets, api):
+def process_import_tree(b2y, budgets, api):
     root = get_import_root()
     jobs = []
     for budget in budgets:
         for account in budget.accounts:
             account_path = os.path.join(root, budget.name, account.name)
+            bank_id = "DK Bankernes EDB Central"
+            config = os.path.join(account_path, CONFIG_FILE)
+            with open(config, "r") as f:
+                bank_id = json.load(f)["bank_id"]
+
             for filename in os.listdir(account_path):
-                jobs.append(Job(budget=budget, account=account, filename=os.path.join(account_path, filename)))
+                jobs.append(Job(budget=budget, account=account, filename=os.path.join(account_path, filename), bank_id = bank_id))
     for job in jobs:
+        bank = b2y.get_bank_by_name(job.bank_id)
+        logging.info("Process job {}".format(job))
         transaction_data = bank.read_data(job.filename)
         if not transaction_data:
             logging.info("No data in file for job: {}".format(job))
@@ -56,20 +72,14 @@ def process_import_tree(bank, budgets, api):
 
 
 def main():
-
     config = get_configs()
-    # print(transaction_data)
     api = YNAB_API(config)
     budgets = api.list_budgets()
     print(budgets)
     init_import_root(budgets)
 
     b2y = Bank2Ynab(config)
-    bank_id = "DK Bankernes EDB Central"
-    my_bank = b2y.get_bank_by_name(bank_id)
-    print(my_bank.name)
-
-    process_import_tree(my_bank, budgets, api)
+    process_import_tree(b2y, budgets, api)
 
 if __name__ == "__main__":
     main()

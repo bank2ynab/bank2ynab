67e36ce655c4afc0d49d40a613e0239c4cf56936
nocalla <nocalla@users.noreply.github.com>
2021-04-10 19:43:22 +0100

    Change if statement to be cleaner

--- bank2ynab/bank_process.py
+++ bank2ynab/bank_process.py
@@ -81,7 +81,7 @@ class B2YBank(object):
                     if f.startswith(file_pattern)
                     if prefix not in f
                 ]
-            if files != [] and missing_dir is True:
+            if not files and missing_dir:
                 s = (
                     "\nFormat: {}\n\nError: Can't find download path: {}"
                     "\nTrying default path instead:\t {}"

dff8e885a585daab34358029ea62385da8b874ba
nocalla <nocalla@users.noreply.github.com>
2021-12-19 15:26:54 +0000

    Restructure test folders

---
+++ test/unit/__init__.py
@@ -0,0 +1,8 @@
+import sys
+from os.path import dirname, join, realpath
+
+project_dirname = dirname(dirname(realpath(__file__)))
+path = join(project_dirname, "bank2ynab")
+
+if path not in sys.path:
+    sys.path.append(path)

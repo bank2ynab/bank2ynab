180caefa450522d119ab428e164eeb632e304658
nocalla <nocalla@users.noreply.github.com>
2021-12-30 13:32:42 +0000

    Switch from tabula-py to pdfplumber for pdf plugin

--- bank2ynab/plugins/pdf_converter.py
+++ bank2ynab/plugins/pdf_converter.py
@@ -1,6 +1,8 @@
+import logging
 import os
 
-import tabula
+import pandas as pd
+import pdfplumber
 from bank_handler import BankHandler
 
 
@@ -13,37 +15,46 @@ class PDF_Converter(BankHandler):
         self.config = config_object
 
     def _preprocess_file(self, file_path: str, plugin_args: list) -> str:
-        """dfs = tabula.read_pdf(
-            file_path,
-            pages="all",
-            output_format="dataframe",
-            multiple_tables=True,
-        )"""
-        # chosen_df = int(plugin_args[0])
+        logging.info("Converting PDF file...")
+        # generate output path
         new_path = get_output_path(file_path, self.config["bank_name"])
-        """ dfs[chosen_df].to_csv(
-            new_path,
-            index=False,
-            encoding="utf-8",
-        ) """
-        tabula.convert_into(
-            file_path,
-            new_path,
-            output_format="csv",
-            pages="all",
-        )
+        # create the pdf object
+        pdf = pdfplumber.open(file_path)
+        # create empty dataframe
+        column_labels = self.config["input_columns"]
+        combined_df = pd.DataFrame(columns=column_labels)
+        # add each page's main table to the dataframe
+        for page in pdf.pages:
 
+            table = page.extract_table()
+            try:
+                # get the main table for a page & set columns
+                page_df = pd.DataFrame(table, columns=column_labels)
+                # if the table has values, add it to the dataframe
+                if not page_df.empty:
+                    combined_df = combined_df.append(
+                        page_df, ignore_index=True
+                    )
+            except ValueError:
+                # if the number of columns isn't right, ignore the table
+                pass
+        # write the dataframe to output file
+        combined_df.to_csv(new_path, index=False)
+        logging.info("\tFinished converting PDF file.")
+        # return the path the the output file
         return new_path
 
 
 def get_output_path(original_path: str, bank_name: str) -> str:
     target_dir = os.path.dirname(original_path)
     new_filename = f"converted pdf statement - {bank_name}.csv"
-    while os.path.isfile(new_filename):
-        counter = 1
+    new_path = os.path.join(target_dir, new_filename)
+    counter = 1
+    while os.path.isfile(new_path):
         new_filename = f"converted pdf statement - {bank_name}_{counter}.csv"
+        new_path = os.path.join(target_dir, new_filename)
         counter += 1
-    return os.path.join(target_dir, new_filename)
+    return new_path
 
 
 def build_bank(config):
---
+++ test/integration/test_plugin_pdf_converter.py
@@ -0,0 +1,12 @@
+from unittest import TestCase
+
+
+class TestDataframeHandler(TestCase):
+    def setUp(self) -> None:
+        return super().setUp()
+
+    def tearDown(self) -> None:
+        return super().tearDown()
+
+    def test_general_test(self):
+        raise NotImplementedError

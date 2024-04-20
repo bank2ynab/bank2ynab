405d785069b078654630b7bea199be01a17234f8
Lim Ding Wen <limdingwen@gmail.com>
2024-04-21 01:25:18 +0800

    Add support for XLS files

---
+++ bank2ynab/plugins/xls_converter.py
@@ -0,0 +1,51 @@
+import logging
+
+import bank_handler
+from pandas import read_excel
+from bank_handler import BankHandler
+
+
+class XLS_Converter(BankHandler):
+    def __init__(self, config_object: dict):
+        """
+        :param config_object: a dictionary of conf parameters
+        """
+        super().__init__(config_object)
+        self.config = config_object
+
+    def _preprocess_file(self, file_path: str, plugin_args: list) -> str:
+        """
+        Combines all tables in a PDF file into one table and writes to CSV.
+
+        :param file_path: path to PDF file
+        :type file_path: str
+        :param plugin_args: plugin arguments (unused in this plugin)
+        :type plugin_args: list
+        :return: path to CSV file
+        :rtype: str
+        """
+        logging.info("Converting XLS file...")
+
+        # create dataframe from pdf
+        df = read_excel(file_path)
+        # generate output path
+        new_path = bank_handler.get_output_path(
+            input_path=file_path,
+            prefix=f"Converted XLS_{self.config['bank_name']}_",
+            ext=".csv",
+        )
+        # write the dataframe to output file
+        df.to_csv(new_path, index=False)
+        logging.info("\tFinished converting XLS file.")
+        return new_path
+
+
+def build_bank(config):
+    """This factory function is called from the main program,
+    and expected to return a BankHandler subclass.
+    Without this, the module will fail to load properly.
+
+    :param config: dict containing all available configuration parameters
+    :return: a BankHandler subclass instance
+    """
+    return XLS_Converter(config)
